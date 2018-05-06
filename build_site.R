b1364a019e3b7c0f0772f25c88aeaad466fbd5b4
Fatih EROL <35523412+ferol15@users.noreply.github.com>
2018-05-06 14:12:42 +0300

    Add files via upload

--- build_site.R
+++ build_site.R
@@ -1,2 +1,7 @@
 setwd("/Users/fatiheminearierol/kisilik_ozellikleri")
 rmarkdown::render_site(encoding = 'UTF-8')
+library(knitr)
+update.packages()
+install.packages('knitr', repos = c('http://rforge.net', 'http://cran.rstudio.org'),
+                 type = 'source')
+
